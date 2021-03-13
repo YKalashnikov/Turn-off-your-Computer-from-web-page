ef19c4f432ccae5d7e8a2ffa4b732b3591cd34c7
YKalashnikov <uk2709@gmail.com>
2021-03-13 15:57:25 -0800

    remove old code

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var powerOff = require('power-off');
+const express = require('express')
+const cors = require('cors')
+const powerOff = require('power-off');
 
-var server = express()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static("./public"))
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(express.static("./public"))
-
-server.post("/shutdown", (req, res, next) => {
+app.post("/shutdown", (req, res, next) => {
 
     powerOff(function (err, stderr, stdout) {
         if (!err && !stderr) {
@@ -17,5 +16,5 @@ server.post("/shutdown", (req, res, next) => {
         res.send("shutting down...")
     });
 })
-
-server.listen(8888, () => { console.log("Server is running") })
+const PORT = 8888
+app.listen(8888, () => { console.log(`Server is running on port ${PORT}`) })
